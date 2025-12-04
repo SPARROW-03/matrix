6d69d24a95cf922c9da7aa413e1b20f4de4e4bfe
SPARROW-03 <vcdevaraj309@gmail.com>
2025-12-04 15:29:10 +0530

    gazebo controller plugin

--- matrix_bot/launch/rsp.launch.py
+++ matrix_bot/launch/rsp.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
+        output='both',
         parameters=[{
             'robot_description': robot_description,
             'use_sim_time': use_sim_time
--- matrix_bot/launch/sim.launch.py
+++ matrix_bot/launch/sim.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
         executable='spawn_entity.py',
         arguments=['-topic','robot_description',
                    '-entity','matrix',
-                   '-x','0.0','-y','0.0','-z','0.05'],
+                   '-x', '0.0', '-y', '0.0', '-z', '0.0'],
         output='screen'
     )
 
