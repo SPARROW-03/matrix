6b05afe17c55c0bdcb7634e244f53a2715a867ec
SPARROW-03 <vcdevaraj309@gmail.com>
2025-12-06 12:29:36 +0530

    changed wheel dimentions

--- matrix_bot/launch/rsp.launch.py
+++ matrix_bot/launch/rsp.launch.py
@@ -1,5 +1,5 @@
 import os
-from ament_index_python import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
