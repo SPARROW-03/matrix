f30e4878d62520a72d9e3e91df5c6bc0d7bf2d24
SPARROW-03 <vcdevaraj309@gmail.com>
2026-08-12 16:05:40 +0530

    updated launch files

--- matrix_bot/launch/bringup.launch.py
+++ matrix_bot/launch/bringup.launch.py
@@ -1,4 +1,5 @@
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
@@ -6,27 +7,24 @@ import os
 
 def generate_launch_description():
     package_dir = get_package_share_directory('matrix_bot')
-    
-    localization = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_dir, 'launch', 'localization.launch.py')
-        )
+
+    rviz_visualization = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(package_dir, 'launch', 'rsp.launch.py'))
     )
 
     navigation = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_dir, 'launch', 'navigation.launch.py')
-        )
+        PythonLaunchDescriptionSource(os.path.join(package_dir, 'launch', 'navigation.launch.py'))
     )
 
-    rviz_visualization = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_dir, 'launch', 'rsp.launch.py')
-        )
+    Location_navigation_node = Node(
+        package='matrix_bot',
+        executable='goal_pose_and_client.py',
+        name='goal_pose_and_client',
+        output='screen',
     )
 
     return LaunchDescription([
         rviz_visualization,
-        TimerAction(period=5.0, actions=[localization]),
-        TimerAction(period=10.0, actions=[navigation])
+        TimerAction(period=5.0, actions=[navigation]), 
+        TimerAction(period=10.0, actions=[Location_navigation_node]),
     ])
